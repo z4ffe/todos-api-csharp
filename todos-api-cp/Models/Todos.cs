667d36661567adbdb9cfa1996fd804771357570c
Pavel Kulyasov <pavel.kulyasov@gmail.com>
2023-08-16 02:18:32 +0300

    init

---
+++ todos-api-cp/Models/Todos.cs
@@ -0,0 +1,16 @@
+﻿namespace TodosApiCp.Models
+
+{
+	public class Todos	
+	{
+		public int Id { get; set; }
+
+		public required string Title { get; set; }
+
+        public required string Text { get; set; }
+
+		public required DateTime CreatedAt { get; set; }
+
+    }
+}
+
